7f7f7983d356e27b82ff602ef935ed13ab687887
Andrea Ballestrazzi <85613584+WinterWind33@users.noreply.github.com>
2023-10-22 01:10:36 +0200

    Add versioning generation from CMake (#10)
    * Adde clang-format
    
    * Updated tasks and launch configurations
    
    * Added initial version generation
    
    * Updated changelog.
    
    * Removed unused include.
    
    * Added settings + format cleaup.
    
    * Try fix compiler version in workflow.
    
    * Try use C++ modules.
    
    * Fix typo.
    
    * Try fix compiler
    
    * Fix option
    
    * Again
    
    * Reverted modules

--- src/entry-point.cpp
+++ src/entry-point.cpp
@@ -1,5 +1,11 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 
+// C++ STL
+#include <iostream>
+
+#include "server-version.hpp"
+
 int main(int argc, char* argv[]) {
+    std::cout << rpic_server::version::getServerVersion() << std::endl;
     return 0;
 }
