5fddec912aa9c00f8c4fbd5c86febdbed696dd2a
Andrea Ballestrazzi <ballestrazziandrea@outlook.com>
2023-10-22 00:38:04 +0200

    Added initial version generation

--- src/entry-point.cpp
+++ src/entry-point.cpp
@@ -1,5 +1,11 @@
 // Copyright (c) 2023 Andrea Ballestrazzi
 
+#include <iostream>
+#include <neargye/semver.hpp>
+
+#include "server-version.hpp"
+
 int main(int argc, char* argv[]) {
+    std::cout << rpic_server::version::getServerVersion() << std::endl;
     return 0;
 }
